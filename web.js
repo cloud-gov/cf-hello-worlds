c544fe1455c47b206093b15e5daf68c671858292
Jonathan Rudenberg <jonathan@titanous.com>
2013-11-13 23:27:01 -0500

    Remove express

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
+var http = require('http');
 var port = process.env.PORT || 5000;
-app.get('/', function(request, response) {
-  response.send('Hello World from port '+port+'!');
-});
 
-app.listen(port, function() {
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from port '+port+'\n');
+}).listen(port, function() {
   console.log("Listening on " + port);
 });

e2ec767cd4b04965b3501a2db8f412d636b767db
Jonathan Rudenberg <jonathan@titanous.com>
2014-04-20 20:29:52 -0400

    Update output
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from port '+port+'\n');
+  res.end('Hello Flynn on port '+port+'\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });

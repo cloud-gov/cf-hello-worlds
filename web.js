b388ce75394636c049525ed71a339632f6504190
Jonathan Rudenberg <jonathan@titanous.com>
2014-09-17 21:22:23 -0400

    Add hostname to response
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Flynn on port '+port+'\n');
+  res.end('Hello from Flynn on port '+port+' from container '+process.env.HOSTNAME+'\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });

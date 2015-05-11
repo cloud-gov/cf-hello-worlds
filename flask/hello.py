df1ba9d95bed54ae36ecff88be4a9e1da65b8b84
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-11 14:35:14 -0500

    add flask app from https://github.com/michaljemala/hello-python

---
+++ flask/hello.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return 'Hello World!'
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))

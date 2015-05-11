12d4b1a28b6f3e56729dbc23007959efc11e5729
Aidan Feldman <aidan.feldman@gsa.gov>
2015-05-11 14:36:14 -0500

    minor tweaks to flask app

--- flask/hello.py
+++ flask/hello.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+	return 'Hello World from Flask!'
 
-port = os.getenv('VCAP_APP_PORT', '5000')
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))

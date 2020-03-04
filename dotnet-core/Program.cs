dbd50213b9b159a4dacf154afd60f3b6a6776b00
Mark Headd <mheadd@gmail.com>
2020-03-04 09:33:36 -0500

    Update Program.cs
    Update dotnet core sample to version 3.1

--- dotnet-core/Program.cs
+++ dotnet-core/Program.cs
@@ -1,26 +1,20 @@
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
-namespace HelloWeb
+namespace dot_net_gov
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
-            var config = new ConfigurationBuilder()
-                          .AddCommandLine(args)
-                          .Build();
-            var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }

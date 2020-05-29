e67348c21fe0634011a5f2b90bbe51a639b05b8e
Mark Headd <mheadd@gmail.com>
2020-05-29 16:53:43 -0400

    Update node example to use more recent JS lajnguage constructs

--- nodejs/web.js
+++ nodejs/web.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var os = require('os');
-var port = process.env.PORT || 5000;
+const http = require('http');
+const os = require('os');
+const port = process.env.PORT || 5000;
 
-http.createServer(function (req, res) {
+http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from NodeJS on port ' + port + ' from container ' + os.hostname() + '\n');
-}).listen(port, function() {
+  res.end(`Hello World from NodeJS on port ${port} from container ${os.hostname()}`);
+}).listen(port, () => {
   console.log("Listening on " + port);
 });

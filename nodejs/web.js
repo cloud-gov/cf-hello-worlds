03963f506768a83fc97811c40ef1847948d448de
Mark Boyd <mark.boyd@gsa.gov>
2023-10-18 15:45:31 -0400

    add bunyan package and test custom logs

--- nodejs/web.js
+++ nodejs/web.js
@@ -1,10 +1,14 @@
 const http = require('http');
 const os = require('os');
+const bunyan = require('bunyan');
+
 const port = process.env.PORT || 5000;
+const logger = bunyan.createLogger({name: "myapp"});
 
 http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  logger.info({baltimore: "orioles"});
   res.end(`Hello World from NodeJS on port ${port} from container ${os.hostname()}`);
 }).listen(port, () => {
-  console.log("Listening on " + port);
+  logger.info({"message":"Listening on " + port});
 });

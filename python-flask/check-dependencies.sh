bda96dd8a05d4fb3a7577cf54919b97d3f352b27
Robert Gottlieb <robert.gottlieb@gsa.gov>
2024-02-07 17:12:40 -0500

    hopefully fixed path this time

--- python-flask/check-dependencies.sh
+++ python-flask/check-dependencies.sh
@@ -4,6 +4,6 @@ echo "about to compile requirements.in"
 pip3 install pip-tools
 python3 -m piptools compile \
 --quiet \
---output-file=requirements.txt \
-./requirements.in
+--output-file=cf-hello-worlds/python-flask/requirements.txt \
+cf-hello-worlds/python-flask/requirements.in
 echo "done compiling requirements"

c987de5c4105c5af0dfea2063032f99598af247f
David Anderson <david.n.anderson@gsa.gov>
2024-07-08 17:52:46 -0400

    use general-task image and install jdk

--- java-see/build-java-see.sh
+++ java-see/build-java-see.sh
@@ -4,6 +4,15 @@ set -ex
 
 export TERM=${TERM:-dumb}
 
+wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.tar.gz
+tar -xvf jdk-22_linux-x64_bin.tar.gz
+mv jdk-22.0.1 /opt/
+
+JAVA_HOME='/opt/jdk-22.0.1'
+export JAVA_HOME
+PATH="JAVA_HOME/bin:$PATH"
+export PATH
+
 pushd cf-hello-worlds/java-see
   ./mvnw compile
   ./mvnw package

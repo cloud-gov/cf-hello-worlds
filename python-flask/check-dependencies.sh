37d70adf360c9c49613375f838a305be54da4135
Peter Burkholder <peter.burkholder@gsa.gov>
2025-05-01 09:34:19 -0400

    Simplify README to the bare minimum

--- python-flask/check-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "about to compile requirements.in"
-pip3 install pip-tools
-python3 -m piptools compile \
---quiet \
---output-file=cf-hello-worlds/python-flask/requirements.txt \
-cf-hello-worlds/python-flask/requirements.in
-echo "done compiling requirements"

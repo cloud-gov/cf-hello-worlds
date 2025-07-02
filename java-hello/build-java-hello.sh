9d3a9d53fb55ba5b9d828b4af66164380f84bd1a
Mark Boyd <mark.boyd@gsa.gov>
2025-07-02 11:53:33 -0400

    rename java-see -> java-hello

--- java-hello/build-java-hello.sh
+++ java-hello/build-java-hello.sh
@@ -7,14 +7,14 @@ export TERM=${TERM:-dumb}
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 tar -xvf jdk-21_linux-x64_bin.tar.gz
 VERSION=$(find . -maxdepth 1 -mindepth 1 -name  "jdk-21.*" | sed 's/^..//')
-mv $VERSION /opt/
+mv "$VERSION" /opt/
 
 JAVA_HOME="/opt/${VERSION}"
 export JAVA_HOME
 PATH="JAVA_HOME/bin:$PATH"
 export PATH
 
-pushd cf-hello-worlds/java-see
+pushd cf-hello-worlds/java-hello
   ./mvnw compile
   ./mvnw package
 popd

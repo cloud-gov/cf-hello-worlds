7b568eaf6dd25ea5f1511ceea4b574368fb9abb0
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-02-04 10:25:09 -0700

    add concourse build task

---
+++ java-spring-actuator/build-java-spring-actuator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+export TERM=${TERM:-dumb}
+
+wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
+tar -xvf jdk-21_linux-x64_bin.tar.gz
+VERSION=$(find . -maxdepth 1 -mindepth 1 -name  "jdk-21.*" | sed 's/^..//')
+mv "$VERSION" /opt/
+
+JAVA_HOME="/opt/${VERSION}"
+export JAVA_HOME
+PATH="JAVA_HOME/bin:$PATH"
+export PATH
+
+pushd cf-hello-worlds/java-spring-actuator
+  ./mvnw compile
+  ./mvnw package
+popd
+
+cp -r cf-hello-worlds/* cf-hello-worlds-jsa-build/
